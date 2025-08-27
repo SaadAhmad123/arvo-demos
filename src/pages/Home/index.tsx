06b20dd354e8f00c26aaa7468f029a5e9c0bede3
Saad Ahmad <saad.ahmad@bain.com>
2025-08-27 21:31:01 +0800

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,11 +5,11 @@ import { withNavBar } from '../../components/Navbar/withNavBar';
 import { Separator } from '../../components/Separator';
 import { CTA } from './components/CTA';
 import { Hero } from './components/Hero';
-import {} from 'react-icons/hi';
 import { Installation } from './components/Installation';
 import { useMount } from '../../hooks/useMount';
 import { Pillers } from './components/Pillars';
 import { Demo } from './components/Demo';
+import { UnderConstruction } from './UnderConstruction';
 
 export const HomePage = withNavBar(() => {
   useMount(() => {
@@ -23,6 +23,8 @@ export const HomePage = withNavBar(() => {
     <main>
       <Separator padding={8} />
       <ContentContainer>
+        <UnderConstruction />
+        <Separator padding={8} />
         <section
           className='grid grid-cols-1 lg:grid-cols-2 gap-2 min-h-[600px] lg:min-h-[500px]'
           aria-labelledby='hero-title'
